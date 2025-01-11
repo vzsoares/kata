40e3b36248ffb903f1175ecb685af1c253492929
vzsoares <vinicius.zenha@gmail.com>
2025-01-10 21:16:41 -0300

    check

--- SearchinRotatedSortedArray/go/unk/main.go
+++ SearchinRotatedSortedArray/go/unk/main.go
@@ -9,14 +9,29 @@ func search(nums []int, target int) int {
 	for start <= end {
 		mid := ((end - start) / 2) + start
 
-		if nums[mid] == target {
-			return mid
-		} else if nums[mid] < target {
+		if nums[end] <= nums[mid] {
 			start = mid + 1
 		} else {
-			end = mid - 1
+			end = mid
 		}
 
+	}
+	println("start end", start, end)
+	println("pivot", start)
+	pivot := start
+	lo := 0
+	hi := l - 1
+	for lo <= hi {
+		m := (hi + lo) / 2
+		rm := (m + pivot) % l
+
+		if nums[rm] == target {
+			return rm
+		} else if nums[rm] > target {
+			hi = m - 1
+		} else {
+			lo = m + 1
+		}
 	}
 
 	return -1
@@ -24,8 +39,18 @@ func search(nums []int, target int) int {
 
 func main() {
 	println(search([]int{0, 1, 3, 5, 6, 8, 12, 15}, 3), "should 2")
+	println("")
 	println(search([]int{0, 1, 3, 5, 6, 8, 12, 15}, 15), "should 7")
+	println("")
 	println(search([]int{4, 5, 6, 7, 0, 1, 2}, 0), "should 4")
+	println("")
+	println(search([]int{6, 7, 0, 1, 2, 4, 5}, 0), "should 2")
+	println("")
 	println(search([]int{4, 5, 6, 7, 0, 1, 2}, 3), "should -1")
+	println("")
 	println(search([]int{1}, 0), "should -1")
+	println("")
+	println(search([]int{5, 1, 3}, 5), "should 0")
+	println("")
+	println(search([]int{4, 5, 6, 7, 0, 1, 2}, 0), "should 4")
 }
