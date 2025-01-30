641e4c90950861f64e0eca38c1a154b460685715
vzsoares <vinicius.zenha@gmail.com>
2025-01-29 21:45:40 -0300

    cuezy

--- FindMinimuminRotatedSortedArrayII/go/logn/main.go
+++ FindMinimuminRotatedSortedArrayII/go/logn/main.go
@@ -6,51 +6,37 @@ func findMin(nums []int) int {
 	l := len(nums)
 	lo, hi := 0, l-1
 
-	inOnce := false
 	for lo < hi {
 		mid := ((hi - lo) / 2) + lo
 
-		if nums[hi] == nums[mid] && ((hi - mid) > 1) && !inOnce {
-			//find if are all equal
-			loI := mid
-			hiI := hi
-			println("IN", loI, hiI)
-			// panic("")
-			for loI < hiI {
-				midI := ((hiI - loI) / 2) + loI
-				if nums[midI] < nums[hiI] {
-					//if found lesser value set lo
-					loI = loI + 1
-					lo = midI
-				} else {
-					hiI = hiI - 1
-				}
-				println("IN", loI, hiI, midI)
-				inOnce = true
-			}
-		} else if nums[hi] < nums[mid] {
+		if nums[mid] < nums[hi] {
+			hi = mid
+		} else if nums[mid] > nums[hi] {
 			lo = mid + 1
 		} else {
-			hi = mid
+			hi--
 		}
-		println("OUT")
 
 	}
 
-	return nums[lo]
+	return nums[hi]
 }
 
 func main() {
-	// println(findMin([]int{1, 3, 5}), "should 1")
-	// println("")
-	// println(findMin([]int{2, 2, 2, 0, 1}), "should 0")
-	// println("")
-	// println(findMin([]int{3, 3, 1, 3}), "should 1")
-	// println("")
-	// println(findMin([]int{1, 3, 3}), "should 1")
-	// println("")
-	// println(findMin([]int{-1, -1, -1, -1}), "should -1")
-	// println("")
+	println(findMin([]int{1, 3, 5}), "should 1")
+	println("")
+	println(findMin([]int{2, 2, 2, 0, 1}), "should 0")
+	println("")
+	println(findMin([]int{3, 3, 1, 3}), "should 1")
+	println("")
+	println(findMin([]int{1, 3, 3}), "should 1")
+	println("")
+	println(findMin([]int{-1, -1, -1, -1}), "should -1")
+	println("")
 	println(findMin([]int{3, 3, 3, 3, 3, 3, 3, 3, 1, 3}), "should 1")
+	println("")
+	println(findMin([]int{3, 3, 1, 3, 3, 3, 3}), "should 1")
+	println("")
+	println(findMin([]int{3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 2, 3}), "should 1")
 	println("")
 }
