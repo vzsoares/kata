48ba35735e1e3453130e7ea514fa08cfe34f2db9
vzsoares <vinicius.zenha@gmail.com>
2026-07-07 22:58:41 -0300

    RotateArray by swap

--- RotateArray/go/2/main.go
+++ RotateArray/go/2/main.go
@@ -3,19 +3,13 @@ package main
 import "fmt"
 
 func rotate(nums []int, k int) {
+	k = k % len(nums)
 
-	start := k % len(nums)
-	if start == 0 {
-		return
-	}
-	cp := make([]int, len(nums))
-	copy(cp, nums)
-
-	for i := range nums {
-		nums[start] = cp[i]
-		start++
-		if start >= len(nums) {
-			start = 0
+	for range k {
+		for i := len(nums) - 1; i >= 1; i-- {
+			tmp := nums[i-1]
+			nums[i-1] = nums[i]
+			nums[i] = tmp
 		}
 	}
 }
@@ -23,5 +17,22 @@ func rotate(nums []int, k int) {
 func main() {
 	arr := []int{1, 2, 3, 4, 5, 6, 7}
 	rotate(arr, 3)
+	fmt.Println("[5,6,7,1,2,3,4]")
+	fmt.Println(arr)
+
+	arr = []int{-1, -100, 3, 99}
+	rotate(arr, 2)
+	fmt.Println("[3,99,-1,-100]")
+	fmt.Println(arr)
+
+	arr = []int{1, 2, 3}
+	rotate(arr, 1)
+	fmt.Println("[3,1,2]")
+	fmt.Println(arr)
+
+	arr = []int{1, 2}
+	rotate(arr, 1)
+	fmt.Println("[2,1]")
 	fmt.Println(arr)
 }
+
