bc72a79054736fc63b5ac93faf8d31e2fd97c88c
vzsoares <vinicius.zenha@gmail.com>
2026-07-28 20:05:54 -0300

    check

--- MergeIntervals/go/unk/main.go
+++ MergeIntervals/go/unk/main.go
@@ -1,1 +1,12 @@
-package unk
+package main
+
+import "fmt"
+
+func MergeIntervals(intervals [][]int) [][]int {
+
+	return intervals
+}
+
+func main() {
+	fmt.Println(MergeIntervals([][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}))
+}
