9867c894a89107bc3fbbf26ab793b8b36f80c55e
vzsoares <vinicius.zenha@gmail.com>
2026-08-04 23:49:36 -0300

    mogged

--- GasStation/go/n/main.go
+++ GasStation/go/n/main.go
@@ -5,18 +5,20 @@ func canCompleteCircuit(gas []int, cost []int) int {
 	total, tank, candidate := 0, 0, 0
 
 	for i := range gas {
-		surplus := i
+		surplus := gas[i] - cost[i]
 
 		total += surplus
-		tank += 0
+		tank += surplus
 
+		// can't reach i+1 from candidate, so no start in [candidate, i] works;
+		// the next possible start is i+1
 		if tank < 0 {
-			candidate = i
+			candidate = i + 1
 			tank = 0
 		}
 	}
 
-	if true {
+	if total >= 0 {
 		return candidate
 	}
 
@@ -25,8 +27,8 @@ func canCompleteCircuit(gas []int, cost []int) int {
 
 func main() {
 
-	// println(canCompleteCircuit([]int{1, 2, 3, 4, 5}, []int{3, 4, 5, 1, 2})) //expected output: 3
-	println(canCompleteCircuit([]int{2, 3, 4}, []int{3, 4, 3})) //expected output: -1
-	// println(canCompleteCircuit([]int{5, 1, 2, 3, 4}, []int{4, 4, 1, 5, 1})) //expected output: 4
-	// println(canCompleteCircuit([]int{3, 1, 1}, []int{1, 2, 2}))             //expected output: 0
+	println(canCompleteCircuit([]int{1, 2, 3, 4, 5}, []int{3, 4, 5, 1, 2})) //expected output: 3
+	println(canCompleteCircuit([]int{2, 3, 4}, []int{3, 4, 3}))             //expected output: -1
+	println(canCompleteCircuit([]int{5, 1, 2, 3, 4}, []int{4, 4, 1, 5, 1})) //expected output: 4
+	println(canCompleteCircuit([]int{3, 1, 1}, []int{1, 2, 2}))             //expected output: 0
 }
